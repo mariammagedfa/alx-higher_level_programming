5e13b114baef72e68e3009200be9e25df2d9ac4e
mariammagedfa <mariammagedfa@yahoo.com>
2023-07-11 15:00:44 -0700

    0

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,50 +1,23 @@
 #!/usr/bin/python3
 """
-script that adds command-line arguments to a Python list and
-saves them to a file:
+    This is a python script that adds all arguments to a Python List.
+    List is then saved to a file.
 """
+
 import sys
-from os import path
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-filename = "add_item.json"
-"""
-# Write a class Student that defines a student by:
-# VARIABLE(" "):
-# To JSON(self): Student to JSON
-# Return: Always 0. (Success)
-"""
-"""
-The script starts by importing the sys module for accessing command-line
-arguments and the path module from the os package for working with file paths.
-"""
-"""
-The save_to_json_file function from the 5-save_to_json_file.py module and the
-load_from_json_file function from the 6-load_from_json_file.py module
-are imported.
-"""
-# Check if the file exists
-if path.exists(filename):
-    # Load the existing list from the file
-    my_list = load_from_json_file(filename)
-else:
-    # Create an empty list if the file doesn't exist
-    my_list = []
+if __name__ == "__main__":
 
-# Add the command-line arguments to the list
-my_list.extend(sys.argv[1:])
+    filename = "add_item.json"
 
-# Save the updated list to the file
-save_to_json_file(my_list, filename)
-"""
-The variable filename is set to "add_item.json", which represents the name of
-the file to save the list. The script checks if the file exists using the
-path.exists() function. If the file exists, the existing list is loaded from
-the file using the load_from_json_file function. If the file doesn't exist,
-an empty list is created. The command-line arguments are obtained using
-sys.argv[1:], which excludes the script name itself (sys.argv[0]). These
-arguments are added to the list using the extend() method. The updated list
-is then saved to the file using the save_to_json_file function.
-"""
+    try:
+        arg_list = load_from_json_file(filename)
+    except:
+        arg_list = []
+
+    for arg in sys.argv[1:]:
+        arg_list.append(arg)
+    save_to_json_file(arg_list, filename)
